4eae8ce4726c63c2a225673f3c140b47370d8d4f
madmatrix <benjamin.wardemann@t-online.de>
2019-03-05 19:49:56 +0100

    Initial commit

---
+++ src/main/java/is/svartifoss/jaxb2/plugin/pluralize/PluralizePlugin.java
@@ -0,0 +1,54 @@
+package is.svartifoss.jaxb2.plugin.pluralize;
+
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.Plugin;
+import com.sun.tools.xjc.model.CClassInfo;
+import com.sun.tools.xjc.model.CPropertyInfo;
+import com.sun.tools.xjc.model.Model;
+import com.sun.tools.xjc.outline.Outline;
+import org.atteo.evo.inflector.English;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+
+import java.util.Collection;
+
+public class PluralizePlugin extends Plugin {
+
+    private static final String OPTION_NAME = "Xpluralize";
+    private static final String USAGE = "\t-Xpluralize\tenable";
+
+    @Override
+    public String getOptionName() {
+        return OPTION_NAME;
+    }
+
+    @Override
+    public String getUsage() {
+        return USAGE;
+    }
+
+    @Override
+    public void postProcessModel(Model model, ErrorHandler errorHandler) {
+        model.beans()
+             .values()
+             .stream()
+             .map(CClassInfo::getProperties)
+             .flatMap(Collection::stream)
+             .filter(CPropertyInfo::isCollection)
+             .forEach(this::pluralize);
+    }
+
+    private void pluralize(final CPropertyInfo propertyInfo) {
+        propertyInfo.setName(true, pluralize(propertyInfo.getName(true)));
+        propertyInfo.setName(false, pluralize(propertyInfo.getName(false)));
+    }
+
+    private String pluralize(final String string) {
+        return English.plural(string);
+    }
+
+    @Override
+    public boolean run(final Outline outline, final Options options, final ErrorHandler errorHandler) throws SAXException {
+        return true;
+    }
+}
---
+++ src/test/java/is/svartifoss/jaxb2/plugin/pluralize/PluralizePluginTest.java
@@ -0,0 +1,55 @@
+package is.svartifoss.jaxb2.plugin.pluralize;
+
+import com.sun.tools.xjc.Driver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PluralizePluginTest {
+
+    private Path directory;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        directory = Paths.get("target", "dummy");
+        if (!Files.exists(directory)) {
+            Files.createDirectory(directory);
+        }
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        if (Files.exists(directory)) {
+            Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    Files.delete(file);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                    Files.delete(dir);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        runXjc("Test1");
+        assertTrue(Files.exists(Paths.get("target", "dummy", "generated", "TestType.java")));
+    }
+
+    private void runXjc(final String name) throws Exception {
+        final String[] args = {"-classpath", "target/xjc-pluralize-1.0-SNAPSHOT.jar", "-Xpluralize", "-d", directory.toString(), "src/test/resources/" + name + ".xsd"};
+        Driver.run(args, System.out, System.out);
+    }
+}
